93ddc442632cc6a988ffbd2043056281c1a2a83e
Vrashabh <86019029+Vrashabh-Sontakke@users.noreply.github.com>
2023-02-08 01:50:24 +0530

    add terraform remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "myapp-bucket"
+        key = "myapp/state.tfstate"
+        region = var.region
+    }
+}
+
 provider "aws" {
     region = var.region
 }
